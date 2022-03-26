043797d55dca89729e9a3e5fd1ba3d06842c7dfb
puterinuralisa <puterinuralisa@graduate.utm.my>
2022-03-26 18:25:14 +0800

    updated config page

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
-<?php 
+<!-- PUTERI NUR ALISA BINTI ISMAIL
+A19ET0362
+SECJ3104 APPLICATION DEVELOPMENT 
+INDIVIDUAL TASK 1 -->
 
+<?php 
 $server = "localhost";
 $user = "root";
 $pass = "";
 $database = "registrationaccount";
-
 $conn = mysqli_connect($server, $user, $pass, $database);
-
 if (!$conn) {
     die("<script>alert('Connection Failed.')</script>");
 }
-
 ?>
