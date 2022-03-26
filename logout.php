cfe03e1e1fe18e6e163b7480bc45a4e619ae3f72
puterinuralisa <puterinuralisa@graduate.utm.my>
2022-03-26 18:24:54 +0800

    updated logout page

--- logout.php
+++ logout.php
@@ -1,7 +1,10 @@
-<?php 
+<!-- PUTERI NUR ALISA BINTI ISMAIL
+A19ET0362
+SECJ3104 APPLICATION DEVELOPMENT 
+INDIVIDUAL TASK 1 -->
 
+<?php 
 session_start();
 session_destroy();
-
 header("Location: index.php");
 ?>
